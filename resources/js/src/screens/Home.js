c34a19eefcdace0fada0cf2823ebf0c7fd5d90e0
Setneb <caiocbm0900@gmail.com>
2021-04-09 17:14:26 -0300

    🖼 Add Routes, Screens (Home e Students) e Navbar

--- resources/js/src/screens/Home.js
+++ resources/js/src/screens/Home.js
@@ -1,80 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import AppContainer from '../components/AppContainer';
-import api from '../api/api';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Layout from '../components/Navbar'
 
-const Home = () => {
-    const [posts, setPosts] = useState(null);
-    const fetchPosts = () => {
-        api.getAllPosts().then(res => {
-            console.log('Getting all Posts...');
-            const result = res.data.data;
-            setPosts(result);
-        });
-    };
-    useEffect(() => {
-        fetchPosts();
-    }, []);
+const useStyles = makeStyles((theme) => ({
+    img: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+        backgroundColor: '#C4C4C4',
+        width: '100%',
+        height: 200,
 
-    const renderPosts = () => {
-        if (!posts) {
-            return (
-                <tr colSpan="4">
-                    Loading...
-                </tr>
-            );
-        }
-        if (posts.length === 0) {
-            return (
+        color: '#000',
+        fontSize: 75,
+    },
+}));
 
-                <tr colSpan="4">
-                    There's no post yet. Please add One :)...
-                </tr>
-            );
-        }
+export default function Home() {
+    const classes = useStyles();
 
-
-        return posts.map((post) => (
-            <tr key={post.id}>
-                <td>{post.id}</td>
-                <td>{post.title}</td>
-                <td>{post.description}</td>
-                <td>
-                    <Link to={`/home/edit/${post.id}`} className="btn btn-warning">EDIT</Link>
-                    <button onClick={() => {
-                        api.deletePost(post.id).then(
-                            fetchPosts()
-                        ).catch(err => {
-                            alert('Failed To Delete');
-                        })
-                    }} className="btn btn-danger">DELETE</button>
-                </td>
-            </tr>
-        ));
-    };
     return (
-        <AppContainer
-            title="Teste"
-        >
-            <Link to="/home/add" className="btn btn-primary float-right">Add New</Link>
-            <div className="table-responsive">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>ID.</th>
-                            <th>Title</th>
-                            <th>Description</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {renderPosts()}
+        <div>
+            <Layout>
 
-                    </tbody>
-                </table>
-            </div>
-        </AppContainer>
-    );
+            </Layout>
+            <Container>
+                <div className={classes.img}>
+                    IMG
+                </div>
+            </Container>
+        </div>
+    )
 }
-
-export default Home;
---
+++ resources/js/src/screens/technician/Students.js
@@ -0,0 +1,278 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import { Container } from '@material-ui/core';
+import Navbar from '../../components/Navbar';
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Cupcake', 305, 3.7, 67, 13),
+    createData('Donut', 452, 25.0, 51, 12),
+    createData('Eclair', 262, 16.0, 24, 11),
+    createData('Frozen yoghurt', 159, 6.0, 24, 10),
+    createData('Gingerbread', 356, 16.0, 49, 9),
+    createData('Honeycomb', 408, 3.2, 87, 8),
+    createData('Ice cream sandwich', 237, 9.0, 37, 7),
+    createData('Jelly Bean', 375, 0.0, 94, 6),
+    createData('KitKat', 518, 26.0, 65, 5),
+    createData('Lollipop', 392, 0.2, 98, 4),
+    createData('Marshmallow', 318, 0, 81, 3),
+    createData('Nougat', 360, 19.0, 9, 2),
+    createData('Oreo', 437, 18.0, 63, 1),
+];
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
+    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
+    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
+    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+];
+
+function EnhancedTableHead(props) {
+    const { classes, order, orderBy, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(5),
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1),
+    },
+    highlight:
+        theme.palette.type === 'light'
+            ? {
+                color: theme.palette.secondary.main,
+                backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+            }
+            : {
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.secondary.dark,
+            },
+    title: {
+        flex: '1 1 100%',
+    },
+}));
+
+const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+
+    return (
+        <Toolbar
+            className={classes.root}
+        >
+            <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+                Nutrition
+            </Typography>
+            <Tooltip title="Filter list">
+                <IconButton aria-label="filter list">
+                    <FilterListIcon />
+                </IconButton>
+            </Tooltip>
+        </Toolbar>
+    );
+};
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    paper: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    },
+    table: {
+        minWidth: 750,
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+    },
+}));
+
+export default function Students() {
+    const classes = useStyles();
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+    return (
+        <div className={classes.root}>
+            <Navbar />
+            <Container>
+                <Paper className={classes.paper}>
+                    <EnhancedTableToolbar />
+                    <TableContainer>
+                        <Table
+                            className={classes.table}
+                            aria-labelledby="tableTitle"
+                            size={'medium'}
+                            aria-label="enhanced table"
+                        >
+                            <EnhancedTableHead
+                                classes={classes}
+                                order={order}
+                                orderBy={orderBy}
+                                onRequestSort={handleRequestSort}
+                                rowCount={rows.length}
+                            />
+                            <TableBody>
+                                {stableSort(rows, getComparator(order, orderBy))
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row, index) => {
+                                        const labelId = `enhanced-table-checkbox-${index}`;
+
+                                        return (
+                                            <TableRow
+                                                hover
+                                                role="checkbox"
+                                                tabIndex={-1}
+                                                key={row.name}
+                                            >
+                                                <TableCell padding="checkbox">
+                                                </TableCell>
+                                                <TableCell component="th" id={labelId} scope="row" padding="none">
+                                                    {row.name}
+                                                </TableCell>
+                                                <TableCell align="right">{row.calories}</TableCell>
+                                                <TableCell align="right">{row.fat}</TableCell>
+                                                <TableCell align="right">{row.carbs}</TableCell>
+                                                <TableCell align="right">{row.protein}</TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                {emptyRows > 0 && (
+                                    <TableRow style={{ height: 53 * emptyRows }}>
+                                        <TableCell colSpan={6} />
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onChangePage={handleChangePage}
+                        onChangeRowsPerPage={handleChangeRowsPerPage}
+                    />
+                </Paper>
+
+            </Container>
+        </div>
+    );
+}
