7f5075a56d078fd3dcfabcf32e6348f6506e202e
Yhan17 <yhannunesleal@gmail.com>
2021-05-26 14:26:49 -0300

    🔨 Arruma Menu Lista de Estudantes KAKA (So não sei o que eu fiz)

--- resources/js/src/screens/process/DocumentSign.js
+++ resources/js/src/screens/process/DocumentSign.js
@@ -1,78 +1,10 @@
 import React from 'react'
 import WebViewer from '@pdftron/webviewer';
 import { useLocation, useHistory } from 'react-router-dom';
-import { makeStyles } from "@material-ui/core/styles";
-import './App.css';
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: '0 8px',
-  },
-  container: {
-    width: '100%',
-    display: 'block',
-    boxSizing: 'border-box',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    height: '100%'
-  },
-  card: {
-    marginTop: theme.spacing(5),
-    padding: theme.spacing(2),
-    minHeight: 640,
-    margin: '0 128px'
-  },
-  header: {
-    marginBottom: theme.spacing(2),
-  },
-  status: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  statusText: {
-    marginLeft: theme.spacing(1),
-  },
-  title: {
-    fontSize: '2.25rem',
-  },
-  subtitle: {
-    fontSize: '0.875rem',
-    opacity: 0.5,
-  },
-  knowladgeArea: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  dataWrapper: {
-  },
-  dataHeader: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingBottom: theme.spacing(2),
-    paddingTop: theme.spacing(1),
-  },
-  dataTitle: {
-    opacity: 0.75,
-  },
-  data: {
-    fontSize: '0.75rem',
-    fontWeight: 400,
-  },
-  descriptionWrapper: {
-    paddingLeft: theme.spacing(2),
-  },
-  description: {
-    paddingLeft: theme.spacing(3),
-  },
-  footer: {
-    margin: theme.spacing(3),
-    textAlign: 'right'
-  },
+import './Document.css';
 
-}));
 export default function DocumentSign() {
-  const classes = useStyles();
-
+  const history = useHistory();
   const viewer = React.useRef(null);
   const location = useLocation();
   const link = location.state.link;
