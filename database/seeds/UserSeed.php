5d444194ef9dcc0e6f0f32e97615d83353a290b9
Gelmir <gelmir.elias@solarview.com.br>
2021-05-26 21:53:01 -0300

    BRANCHE TEMPORARY+: Criação dos usuários para testar cada tipo de permissão.

--- database/seeds/UserSeed.php
+++ database/seeds/UserSeed.php
@@ -21,18 +21,48 @@ class UserSeed extends Seeder
             'phone' => '(63) 90000-0000',
             'password' => bcrypt('unitins2021'),
         ]);
+        $user->assignRole('administrador');      
 
+        ////////////////////////////////////////////////////////////
+        $student = User::create([
+            'name' => 'Estutante',
+            'email' => 'estudante@unitins.br',
+            'color' => '#673ab7',
+            'phone' => '(63) 69696-0000',
+            'password' => bcrypt('unitins2021'),
+        ]);
+        $student->assignRole('estudante');
         $student = Student::create([
             'cpf' => '000.000.000-00',
-            'user_id' => $user->id,
+            'user_id' => $student->id,
         ]);
 
-        $professor = Professor::create([
-            'user_id' => $user->id,
+        ////////////////////////////////////////////////////////////
+        $professor_disciplina = User::create([
+            'name' => 'Disciplina',
+            'email' => 'disciplina@unitins.br',
+            'color' => '#673ab7',
+            'phone' => '(63) 66666-9999',
+            'password' => bcrypt('unitins2021'),
+            ]);
+        $professor_disciplina->assignRole('professor_disciplina');
+        $professor_disciplina = Professor::create([
+            'user_id' => $professor_disciplina->id,
         ]);
 
-        $user->assignRole('administrador');
+        ////////////////////////////////////////////////////////////
+        $professor_orientador = User::create([
+            'name' => 'Orientador',
+            'email' => 'orientado@unitins.br',
+            'color' => '#673ab7',
+            'phone' => '(63) 99999-6666',
+            'password' => bcrypt('unitins2021'),
+        ]);
+        $professor_orientador->assignRole('professor_orientador');
+        $professor_orientador = Professor::create([
+            'user_id' => $professor_orientador->id,
+        ]);
 
-        $this->command->info(("User {$user->name} created"));
+        //$this->command->info(("User {$user->name} created"));
     }
 }
